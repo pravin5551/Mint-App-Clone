7e501bdf3c21ac88ee23ec6740ab08eb22ef0e7e
Dheeraj Gupta <38027375+nero002@users.noreply.github.com>
2021-04-19 11:46:43 +0530

    no change

--- Mint/app/src/main/java/com/nero/mint/data/remote/RetrofitGenerator.kt
+++ Mint/app/src/main/java/com/nero/mint/data/remote/RetrofitGenerator.kt
@@ -19,7 +19,7 @@ class RetrofitGenerator {
 
         fun getInstance(): Retrofit {
             val retrofitBuilder = Retrofit.Builder()
-                .baseUrl("http://81ca1531f976.ngrok.io")
+                .baseUrl("https://1d3d44fc5eca.ngrok.io")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build())
             return retrofitBuilder.build()
